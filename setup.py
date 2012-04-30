c77e49aaa61cc88bf932a548064a90a9576313bc
Thomas Aglassinger <roskakori@users.sourceforge.net>
2012-04-30 23:50:13 +0200

    Removed compatibility with Python 2.5 bacause of collections.namedtuple.

--- setup.py
+++ setup.py
@@ -67,7 +67,6 @@ setup(
         "License :: OSI Approved :: BSD License",
         "Natural Language :: English",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 2.5",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Topic :: Software Development :: Bug Tracking",
