16252b53d0517ef4b381a995434ba78ae7b478e7
Thomas Aglassinger <roskakori@users.sourceforge.net>
2012-04-30 23:49:48 +0200

    * Added command line options and config file. * Activated conversion although for now it always uses pretend=True. * Cleaned up logging.

--- tratihubis.py
+++ tratihubis.py
@@ -16,12 +16,12 @@ Usage
 
 Information about Trac tickets to convert has to be provided in a CSV file. To obtain this CSV file, create a
 new Trac query using the SQL statement stored in "query_tickets.sql" and saving the result by clicking
- "Download in other formats: Comma-delimited Text" and choosing for example ``/Users/me/mytool/tickets.csv``
- as output file.
- 
+"Download in other formats: Comma-delimited Text" and choosing for example ``/Users/me/mytool/tickets.csv``
+as output file.
+
 Next create a config file to describe how to login to Github and what to convert. For example, you could
 store the following in ``/Users/me/mytool/tratihubis.cfg``::
- 
+
   [tratihubis]
   user = someone
   password = secret
@@ -66,17 +66,22 @@ Copyright (c) 2012, Thomas Aglassinger. All rights reserved. Distributed under t
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 import codecs
+import collections
+import ConfigParser
 import csv
 import github
 import logging
-import os
+import optparse
+import sys
 
 _log = logging.getLogger('tratihubis')
 
 __version__ = "0.1"
 
+_FakeMilestone = collections.namedtuple('FakeMilestone', ['number', 'title'])
 
-class UTF8Recoder:
+
+class _UTF8Recoder:
     """
     Iterator that reads an encoded stream and reencodes the input to UTF-8
     """
@@ -91,13 +96,13 @@ class UTF8Recoder:
         return result
 
 
-class UnicodeCsvReader:
+class _UnicodeCsvReader:
     """
     A CSV reader which will iterate over lines in the CSV file "f",
     which is encoded in the given encoding.
     """
     def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
-        f = UTF8Recoder(f, encoding)
+        f = _UTF8Recoder(f, encoding)
         self.reader = csv.reader(f, dialect=dialect, **kwds)
 
     def next(self):  # @ReservedAssignment
@@ -108,29 +113,25 @@ class UnicodeCsvReader:
         return self
 
 
-def _githubLogin():
-    result = {'password': None, 'user': None}
-    with open(os.path.expandvars('${HOME}/.gitconfig'), 'rb') as configFile:
-        for line in configFile:
-            line = line.strip('\n\r\t ')
-            for keyword in result.keys():
-                if line.startswith(keyword + " = "):
-                    result[keyword] = line[len(keyword) + 3:]
-    return result['user'], result['password']
-
-
-def _tracTicketMaps():
+def _tracTicketMaps(ticketsCsvPath):
     """
     Sequence of maps where each items describes the relevant fields of each row from the tickets CSV exported
     from Trac.
     """
-    with open(os.path.expandvars("${HOME}/Desktop/cutplace_trac_tickets.csv"), "rb") as  ticketCsvFile:
-        csvReader = UnicodeCsvReader(ticketCsvFile)
+    EXPECTED_COLUMN_COUNT = 9
+    _log.info(u'read ticket details from "%s"', ticketsCsvPath)
+    with open(ticketsCsvPath, "rb") as  ticketCsvFile:
+        csvReader = _UnicodeCsvReader(ticketCsvFile)
         hasReadHeader = False
         for row in csvReader:
+            columnCount = len(row)
+            if columnCount != EXPECTED_COLUMN_COUNT:
+                # TODO: Add row number to error message.
+                raise ValueError(u'CSV row must have %d columns but has %d: %r' %
+                        (EXPECTED_COLUMN_COUNT, columnCount, row))
             if hasReadHeader:
                 ticketMap = {
-                    'id': row[0],
+                    'id': long(row[0]),
                     'type': row[1],
                     'owner': row[2],
                     'reporter': row[3],
@@ -145,45 +146,104 @@ def _tracTicketMaps():
                 hasReadHeader = True
 
 
-def migrateTickets():
-    user, password = _githubLogin()
-    _log.info('connect to github as user %s', user)
-    hub = github.Github(user, password)
-    cutplaceRepo = hub.get_user().get_repo('cutplace')
-    _log.info('connect to repo %s', cutplaceRepo.name)
-    ticketIndex = 0
-    startCreateIndex = 7
-    endCreateIndex = 99
-    existingMilestones = {}
-    for milestone in cutplaceRepo.get_milestones():
-        existingMilestones[milestone.title] = milestone
-    _log.info("existing milestones: %s", existingMilestones)
-    for ticketMap in _tracTicketMaps():
-        if (ticketIndex >= startCreateIndex) and (ticketIndex <= endCreateIndex):
-            title = ticketMap['summary']
+def _createMilestoneMap(repo):
+    result = {}
+    _log.info(u'analyze existing milestones')
+    for milestone in repo.get_milestones():
+        result[milestone.title] = milestone
+        _log.info(u'  %d: %s', milestone.number, milestone.title)
+    return result
+
+
+def migrateTickets(repo, ticketsCsvPath, firstTicketIdToConvert=1, lastTicketIdToConvert=0, pretend=True):
+    issueId = 1
+    existingMilestones = _createMilestoneMap(repo)
+    for ticketMap in _tracTicketMaps(ticketsCsvPath):
+        ticketId = ticketMap['id']
+        title = ticketMap['summary']
+        if (ticketId >= firstTicketIdToConvert) \
+                and ((ticketId <= lastTicketIdToConvert) or (lastTicketIdToConvert == 0)):
             body = ticketMap['description']
-            assignee = ticketMap['owner']
-            milestoneTitle = ticketMap['milestone']
+            assignee = ticketMap['owner'].strip()
+            milestoneTitle = ticketMap['milestone'].strip()
             if len(milestoneTitle) != 0:
                 if milestoneTitle not in existingMilestones:
-                    _log.info(u'create milestone: %s', milestone)
-                    newMilestone = cutplaceRepo.create_milestone(milestoneTitle)
+                    _log.info(u'add milestone: %s', milestoneTitle)
+                    if not pretend:
+                        newMilestone = repo.create_milestone(milestoneTitle)
+                    else:
+                        newMilestone = _FakeMilestone(len(existingMilestones) + 1, milestoneTitle)
                     existingMilestones[milestoneTitle] = newMilestone
                 milestone = existingMilestones[milestoneTitle]
                 milestoneNumber = milestone.number
             else:
                 milestone = None
                 milestoneNumber = 0
-            _log.info(u"create issue #%d: %s", ticketIndex + 1, title)
-            _log.info(u'  %s; %s (%d)', assignee, milestoneTitle, milestoneNumber)
-            _log.info(u'  %r', body)
-            if milestone is None:
-                cutplaceRepo.create_issue(title, body, assignee)
-            else:
-                cutplaceRepo.create_issue(title, body, assignee, milestone.number)
-        ticketIndex += 1
+            _log.info(u"convert ticket #%d to issue #%d: %s", ticketId, issueId, title)
+            _log.info(u'  owner=%s; milestone=%s (%d)', assignee, milestoneTitle, milestoneNumber)
+            if not pretend:
+                if milestone is None:
+                    repo.create_issue(title, body, assignee)
+                else:
+                    repo.create_issue(title, body, assignee, milestone.number)
+            issueId += 1
+        else:
+            _log.info(u'skip ticket #%d: %s', ticketId, title)
 
-        
-if __name__ == '__main__':
+
+def _parsedOptions(arguments):
+    assert arguments is not None
+
+    # Parse command line options.
+    Usage = """usage: %prog [options] CONFIGFILE
+
+    Convert Trac tickets to Github issues."""
+    parser = optparse.OptionParser(
+        usage=Usage,
+        version="%prog " + __version__
+    )
+    parser.add_option("-v", "--verbose", action="store_true", dest="verbose",
+                      help="log all actions performed in console")
+    (options, others) = parser.parse_args(arguments)
+    if len(others) == 0:
+        parser.error("CONFIGFILE must be specified")
+    elif len(others) > 1:
+        parser.error("unknown options must be removed: %s" % others[2:])
+    if options.verbose:
+        _log.setLevel(logging.DEBUG)
+
+    configPath = others[0]
+
+    return configPath
+
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+
+    exitCode = 1
+    try:
+        configPath = _parsedOptions(argv[1:])
+        config = ConfigParser.SafeConfigParser()
+        config.read(configPath)
+        password = config.get('tratihubis', 'password')
+        repoName = config.get('tratihubis', 'repo')
+        ticketsCsvPath = config.get('tratihubis', 'tickets')
+        user = config.get('tratihubis', 'user')
+        _log.info('log on to github as user "%s"', user)
+        hub = github.Github(user, password)
+        _log.info('connect to github repo "%s"', repoName)
+        repo = hub.get_user().get_repo(repoName)
+        migrateTickets(repo, ticketsCsvPath, pretend=True)
+        exitCode = 0
+    except (EnvironmentError, OSError), error:
+        _log.error(error)
+    except KeyboardInterrupt:
+        _log.warning(u"interrupted by user")
+    except Exception, error:
+        _log.exception(error)
+    return exitCode
+
+if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    migrateTickets()
+    sys.exit(main())
